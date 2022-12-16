b7e4453c6fbd164fbb4e347df4b25826a33cb349
aly ahmed <65836451+aly-ahmed97@users.noreply.github.com>
2022-12-16 16:25:50 +0200

    adding solutions

---
+++ 30+ Most   Asked Patterns/Hollow Rectangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	unsigned int UserNo = 0 ;
+	unsigned int Index1 = 0 ;
+	unsigned int Index2 = 0 ;
+	printf("please enter a number\n");
+	scanf("%i",&UserNo);
+	if(UserNo <= 0)
+	{
+		printf("not a valid number");
+	}
+	else
+	{
+		for(Index1 = 1 ; Index1 <= UserNo ; Index1++)
+		{
+			printf("*");
+			for(Index2 = 1 ; Index2 <= UserNo-2 ; Index2++)
+			{
+				if(Index1 == 1 || Index1 == UserNo)
+				{
+					printf("*");
+				}
+				else
+				{
+				    printf(" ");
+				}
+			}
+			printf("*\n");
+		}
+	}
+}
