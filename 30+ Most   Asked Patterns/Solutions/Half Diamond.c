3326b7708a3347080eb4c1e2ed525484b042b271
aly ahmed <65836451+aly-ahmed97@users.noreply.github.com>
2022-12-21 17:46:47 +0200

    Adding More Solutions

---
+++ 30+ Most   Asked Patterns/Solutions/Half Diamond.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int Index1 = 0;
+    unsigned int Index2 = 0;
+    unsigned int UserNo = 0;
+    printf("please enter the number of raws :\n");
+    scanf("%i",&UserNo);
+    for(Index1 = 1 ; Index1 <= UserNo ; Index1++)
+    {
+        for(Index2 = 2 ; Index2 <= Index1 ; Index2++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(Index1 = 1 ; Index1 <= UserNo ; Index1++)
+    {
+        for(Index2 = UserNo-Index1+1 ; Index2 >= 1 ; Index2--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
